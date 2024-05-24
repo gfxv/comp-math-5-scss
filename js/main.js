272a68885ba8c384d2bc8bf692df93f973af7305
gfxv <superfreezu@gmail.com>
2024-05-24 17:54:44 +0300

    added: Gauss method

--- js/main.js
+++ js/main.js
@@ -4,12 +4,10 @@ window.onload = () => {
   const inputValue = document.getElementById("input-value").getBoundingClientRect().width;
   const lagrangeResult = document.getElementById("lagrange-result").getBoundingClientRect().width;
   const newtonDivResult = document.getElementById("newton-div-result").getBoundingClientRect().width;
-  const newtonEndResult = document.getElementById("newton-en d-result").getBoundingClientRect().width;
   const gaussResult = document.getElementById("gauss-result").getBoundingClientRect().width;
 
   document.getElementById("input-placeholder").innerText = "Input value: " + inputValue;
   document.getElementById("lagrange-result-placeholder").innerText = "Lagrange: " + lagrangeResult;
   document.getElementById("newton-div-result-placeholder").innerText = "Newton div: " + newtonDivResult;
-  document.getElementById("newton-end-result-placeholder").innerText = "Newton end: " + newtonEndResult;
   document.getElementById("gauss-result-placeholder").innerText = "Gauss: " + gaussResult;
 }
